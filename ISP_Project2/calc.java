d47041d3caff4e993a936c9f4cfd25db1eb7992b
17robots <mdray@ameritech.net>
2020-10-09 11:34:57 -0400

    Starting Kind Of Over

--- ISP_Project2/calc.java
+++ ISP_Project2/calc.java
@@ -1,47 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Calc {
-  private static void PrintTitle() {
-    System.out.println("Calculator By Matthew Dray");
-    return;
+
+  public static double PerformOperation(double a, double b, String op) {
+    switch (op) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "*":
+        return a * b;
+      case "/":
+        return a / b;
+      case "%":
+        return a % b;
+      default:
+        return -1;
+    }
   }
 
-  private static void PrintMenu() {
-    System.out.println("Available Menu Options");
-    System.out.println("+ - Addition");
-    System.out.println("- - Subtraction");
-    System.out.println("* - Multiplication");
-    System.out.println("/ - Division");
-    System.out.println("% - Modulus");
-    System.out.println("q - Quit");
-    return;
+  public static ArrayList<Double> GetNumbers(Double a, Double b, Scanner in) {
+    ArrayList<Double> returnedList = new ArrayList<Double>();
+    System.out.println("Enter The First Number");
+    try {
+      returnedList.add(in.nextDouble());
+
+    } catch (Exception e) {
+
+    }
+    return returnedList;
   }
 
-  public static void main(String[] args) {
-    String operation;
-    double num1, num2;
-    PrintTitle();
-    Scanner in = new Scanner(System.in);
-    boolean exitMenu = false;
+  public static String GetOperation(Scanner scan) {
+    String returnedString = "";
+    boolean isFinished = false;
     do {
-      PrintMenu();
-      operation = in.next();
-      switch (operation) {
+      System.out.println("Choose Operation");
+      if (returnedString != "") {
+        System.out.println("Current Operation: " + returnedString);
+      }
+      System.out.println("+ - Addition");
+      System.out.println("- - Subtraction");
+      System.out.println("* - Multiplication");
+      System.out.println("/ - Division");
+      System.out.println("% - Modulus");
+      if (returnedString != "") {
+        System.out.println("c - Confirm Operation");
+      }
+      System.out.println("q - quit");
+      returnedString = scan.nextLine();
+      switch (returnedString) {
         case "+":
         case "-":
         case "*":
         case "/":
         case "%":
-
           break;
         case "q":
-          exitMenu = true;
+          isFinished = true;
+          break;
+        case "c":
+          isFinished = true;
           break;
         default:
-          System.out.println("Please Select An Item On The Menu");
+          System.out.println("Please Choose A Valid Menu Option");
+          returnedString = "";
           break;
       }
-    } while (!exitMenu);
+    } while (!isFinished);
+    return returnedString;
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String op;
+    do {
+      op = GetOperation(in);
+      if (!op.equals("q")) {
+
+      }
+    } while (!op.equals("q"));
     in.close();
+    return;
   }
 }
